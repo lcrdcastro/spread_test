9da87b2fae4ace810e867ed82de4bafdb0ed543d
Luiz Claudio Castro <lcrdcastro@gmail.com>
2022-07-14 21:05:49 -0300

    Finishing the parsing argments; Changing the name of program in makefile; Correcting some bugs;

--- src/main.cpp
+++ src/main.cpp
@@ -27,11 +27,8 @@ static size_t writeCallback(char* contents, size_t size, size_t nmemb, char* buf
 
 string getJsonFromApi(string url){
     CURL *curl;
-
     CURLcode res;
-
     string readBuffer;
-
     curl = curl_easy_init();
 
     if(curl){
@@ -47,44 +44,77 @@ string getJsonFromApi(string url){
     return readBuffer;
 }
 
+void printDatafromJson(Json::Value data, bool checkType)
+{
+    vector<string> members = data.getMemberNames();
+    if (members.size()<=2){
+        checkType = false;
+    }
+    if (!checkType){
+        for (unsigned int i=0; i< members.size();i++){
+            if (data[members[i]].size()>0){
+                cout << members[i] << ":"<< endl;
+                Json::Value ratings = data[members[i]];
 
+                for (unsigned int j=0;j<ratings.size();j++){
+                    cout << "    " << (ratings[j])["Source"].asString() << " - ";
+                    cout << (ratings[j])["Value"].asString() << endl;
+                }
+            }
+            else{
+                cout << members[i] << " - " << data[members[i]].asString() <<  endl;
+            }
+        }
+    }
+    else{
+        cout << data["Title"] << " - " << data["Type"] << endl;
+    }
+}
+
+void printHelp(){
+    cout << "Usage:" << endl;
+    cout << "    moviesearch -s PATTERN" << endl;
+    cout << "    moviesearch --search PATTERN" << endl;
+    cout << "    moviesearch -s PATTERN -t" << endl;
+    cout << "    moviesearch --search PATTERN -type" << endl << endl;
+    cout << "Where:" << endl;
+    cout << "    PATTERN - name of a movie in double quotes  Ex: \"Fight Club\"." << endl;
+    cout << "    -t, --type - Returns the type of media Ex: movie , series, ... " << endl;
+
+}
 
 int main(int argv, char** argc){   
-        
-    //Json::Value rootJsonValue;
-    //rootJsonValue["mensagem"] = 
+    string movieName;
+    bool checkType = false;
+    if (argv>1){
+        for (int i=0;i<argv;i++){
+            string arg = argc[i]; 
+            if (arg.compare("-h") ==0 || arg.compare("--help") ==0){
+                printHelp();
+                exit(0);
+            }
+            if (arg.compare("-s") ==0 || arg.compare("--search") ==0){
+                i++;
+                movieName = argc[i];
+            }
+            if (arg.compare("-t") ==0 || arg.compare("--type") ==0){
+                checkType = true;
+            }
+        }
+    }
+    else{
+        cout << "Usage: moviesearch -s PATTERN -t" << endl;
+        cout << "Try: moviesearch --help for more information." << endl;
+        exit(0);
 
-    //http://www.omdbapi.com/?t=fight+club&apikey=c2d2794b
+    }
     
-    string url = (string)"http://www.omdbapi.com/?t=" + (string)"fight+club" + (string)"&apikey=c2d2794b";
+    
+    string url = (string)"http://www.omdbapi.com/?t=" + movieName + (string)"&apikey=c2d2794b";
     string readBuffer;
 
     readBuffer = getJsonFromApi(url);
     Json::Value result = json_decode(readBuffer);
-    //cout << result.toStyledString();
-    vector<string> members = result.getMemberNames();
-
-    for (unsigned int i=0; i< members.size();i++){
-        if (result[members[i]].size()>0){
-            cout << members[i] << ":"<< endl;
-            Json::Value ratings = result[members[i]];
-
-            for (unsigned int j=0;j<ratings.size();j++){
-                cout << "    " << (ratings[j])["Source"].asString() << " - "<< (ratings[j])["Value"].asString() << endl;
-            }
-        }
-        else{
-            cout << members[i] << " - " << result[members[i]].asString() <<  endl;
-        }
-    }
-
-    
-    //cout << readBuffer << endl;
-    // cout << "number of arguments " << argv << " arguments: " <<  endl;
-    // for (int i=0;i< sizeof(argc);i++)
-    // {
-    //     cout<< argc[i] << endl;
-    // }
-
+    printDatafromJson(result,checkType);
     return 0;
 }
